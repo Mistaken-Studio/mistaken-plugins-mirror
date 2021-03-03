07bca999bb93bdfb1e5d808688aa79f011c1d1a2
grzes 1234 <g_sw@poczta.fm>
2021-03-03 13:10:37 +0100

    \n to \\n in RoundLogger

--- Gamer.RoundLogger/RoundLogger.cs
+++ Gamer.RoundLogger/RoundLogger.cs
@@ -46,7 +46,7 @@ namespace Gamer.RoundLoggerSystem
 
         public static void Log(string module, string type, string message)
         {
-            Logs.Add(new LogMessage(DateTime.Now, type, module, message));
+            Logs.Add(new LogMessage(DateTime.Now, type, module, message.Replace("\n", "\\n")));
         }
 
         public static string PlayerToString(this Player player) => player == null ? null : $"{player.Nickname} (ID: {player.Id}|UID: {player.UserId})";
