40e96f5af7f9694cf0b352d0caa92324b503c21c
grzes 1234 <g_sw@poczta.fm>
2021-03-27 18:07:39 +0100

    Memes

--- MistakenSystemsPlugin/Code/CommandsExtender/Commands/ClientCommands/TestCommand.cs
+++ MistakenSystemsPlugin/Code/CommandsExtender/Commands/ClientCommands/TestCommand.cs
@@ -5,6 +5,7 @@ using Exiled.API.Features;
 using Gamer.Utilities;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 namespace Gamer.Mistaken.CommandsExtender.Commands
@@ -44,9 +45,50 @@ namespace Gamer.Mistaken.CommandsExtender.Commands
                     basePos += offset;
                     ItemType.SCP018.Spawn(0, basePos);
                     return new string[] { player.CurrentRoom.Type + "", basePos.x + "", basePos.y + "", basePos.z + "" };
+                case "heh":
+                    var p = Player.Get(args[1]);
+                    //Explore(p.GameObject.transform);
+                    var size = new Vector3(float.Parse(args[2]), float.Parse(args[3]), float.Parse(args[4]));
+                    var tmp = Find(p.GameObject.transform, "Body");
+                    foreach (var item in tmp.Take(tmp.Count - 1))
+                    {
+                        item.localScale = size;
+                    }
+                    Log.Debug(":|");
+                    foreach (Player _p in Player.List)
+                    {
+                        MethodInfo sendSpawnMessage = Server.SendSpawnMessage;
+                        if (sendSpawnMessage != null)
+                        {
+                            sendSpawnMessage.Invoke(null, new object[]
+                            {
+                                p.ReferenceHub.characterClassManager.netIdentity,
+                                _p.Connection
+                            });
+                        }
+                        else
+                            Log.Debug("FOCK OFF");
+                    }
+                    break;
             }
             success = true;
             return new string[] { "HMM" };
+        }
+
+        public void Explore(Transform obj)
+        {
+            for (int i = 0; i < obj.transform.childCount; i++)
+                Explore(obj.transform.GetChild(i));
+            Log.Debug(obj);
+        }
+        public List<Transform> Find(Transform obj, string name)
+        {
+            var tor = new List<Transform>();
+            for (int i = 0; i < obj.transform.childCount; i++)
+                tor.AddRange(Find(obj.transform.GetChild(i), name));
+            if (obj.name == name || true)
+                tor.Add(obj);
+            return tor;
         }
     }
 }
