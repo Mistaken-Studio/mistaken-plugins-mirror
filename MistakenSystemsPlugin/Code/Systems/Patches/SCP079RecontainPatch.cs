b9655a5ea71cfd1fcd5099f2560653ed9f4525b5
grzes 1234 <g_sw@poczta.fm>
2021-03-27 15:39:38 +0100

    SCP 079 Recontamination Patch Fix(?)

--- MistakenSystemsPlugin/Code/Systems/Patches/SCP079RecontainPatch.cs
+++ MistakenSystemsPlugin/Code/Systems/Patches/SCP079RecontainPatch.cs
@@ -12,7 +12,7 @@ using Gamer.Utilities;
 
 namespace Gamer.Mistaken.Systems.Patches
 {
-	//[HarmonyPatch(typeof(Recontainer079), "BeginContainment")]
+	[HarmonyPatch(typeof(Recontainer079), "BeginContainment")]
 	public static class SCP079RecontainPatch
 	{
 		public static bool ErrorMode { get; private set; } = false;
@@ -24,13 +24,14 @@ namespace Gamer.Mistaken.Systems.Patches
 
 		public static bool Prefix(bool forced)
 		{
-			if(Handle.HasValue)
-            {
+			if (Handle.HasValue)
+			{
 				Log.Warn("Duplicate of Recontain Request");
 				return false;
-            }
+			}
 			Log.Debug("RECONT: -2");
-			Handle = Timing.RunCoroutine(Recontain(forced).CancelWith(Recontainer079.singleton.gameObject).RerouteExceptions((ex) =>
+			int rId = RoundPlus.RoundId;
+			Handle = Timing.RunCoroutine(_Recontain(forced, rId).CancelWith(Recontainer079.singleton.gameObject).RerouteExceptions((ex) =>
 			{
 				Log.Error("SCP 079 Recontainment Failed");
 				Log.Error(ex.Message);
@@ -43,6 +44,10 @@ namespace Gamer.Mistaken.Systems.Patches
 					Handle = null;
 					Recontained = true;
 				}));
+			}).Append(() =>
+			{
+				if(rId != RoundPlus.RoundId)
+					Restart();
 			}), Segment.FixedUpdate);
 			Log.Debug("RECONT: -1");
 			return false;
@@ -57,7 +62,7 @@ namespace Gamer.Mistaken.Systems.Patches
 			Recontained = false;
 			ErrorMode = false;
 		}
-
+		[System.Obsolete("It's broken :/")]
 		private static IEnumerator<float> Recontain(bool forced)
 		{
 			Log.Debug("Starting SCP079 Recontainment");
@@ -226,6 +231,107 @@ namespace Gamer.Mistaken.Systems.Patches
 				Log.Error(ex.StackTrace);
 			}
 			Log.Debug("RECONT: 11");
+			Utilities.API.Map.TeslaMode = Utilities.API.TeslaMode.ENABLED;
+			Recontainer079.isLocked = false;
+			Handle = null;
+			yield break;
+		}
+
+		private static IEnumerator<float> _Recontain(bool forced, int rId)
+		{
+			PlayerStats ps = PlayerManager.localPlayer.GetComponent<PlayerStats>();
+			SecondsLeft = 62;
+            #region Wait
+            Waiting = true;
+			while (Cassie.IsSpeaking || Warhead.IsInProgress)
+				yield return float.NegativeInfinity;
+			Waiting = false;
+			if (SecondsLeft <= -1 || rId != RoundPlus.RoundId)
+			{
+				Handle = null;
+				yield break;
+			}
+            #endregion
+            if (!forced)
+			{
+				Respawning.RespawnEffectsController.PlayCassieAnnouncement(string.Concat(new object[]
+				{
+					"JAM_",
+					UnityEngine.Random.Range(0, 70).ToString("000"),
+					"_",
+					UnityEngine.Random.Range(2, 5),
+					" SCP079RECON5"
+				}), false, true);
+			}
+            #region WaitS
+            for (int i = 0; i < 55; i++)
+			{
+				SecondsLeft--;
+				yield return Timing.WaitForSeconds(1);
+			}
+			if (SecondsLeft <= -1 || rId != RoundPlus.RoundId)
+			{
+				Handle = null;
+				yield break;
+			}
+            #endregion
+            #region Wait
+            Waiting = true;
+			while (Cassie.IsSpeaking || Warhead.IsInProgress)
+				yield return float.NegativeInfinity;
+			Waiting = false;
+			if (SecondsLeft <= -1 || rId != RoundPlus.RoundId)
+			{
+				Handle = null;
+				yield break;
+			}
+			#endregion
+			Respawning.RespawnEffectsController.PlayCassieAnnouncement(string.Concat(new object[]
+				{
+				"JAM_",
+				UnityEngine.Random.Range(0, 70).ToString("000"),
+				"_",
+				UnityEngine.Random.Range(1, 4),
+				" SCP079RECON6"
+			}), true, true);
+			Respawning.RespawnEffectsController.PlayCassieAnnouncement((Scp079PlayerScript.instances.Count > 0) ? "SCP 0 7 9 SUCCESSFULLY TERMINATED USING GENERATOR RECONTAINMENT SEQUENCE" : "FACILITY IS BACK IN OPERATIONAL MODE", false, true);
+			#region WaitS
+			for (int i = 0; i < 7; i++)
+			{
+				SecondsLeft--;
+				yield return Timing.WaitForSeconds(1);
+			}
+			if (SecondsLeft <= -1 || rId != RoundPlus.RoundId)
+			{
+				Handle = null;
+				yield break;
+			}
+			#endregion
+			Generator079.Generators[0].ServerOvercharge(10f, true);
+			SecondsLeft = -1;
+			Recontained = true;
+			HashSet<DoorVariant> lockedDoors = new HashSet<DoorVariant>();
+			foreach (DoorVariant doorVariant in UnityEngine.Object.FindObjectsOfType<DoorVariant>())
+			{
+				if (doorVariant is Interactables.Interobjects.BasicDoor && doorVariant.TryGetComponent(out Scp079Interactable scp079Interactable) && scp079Interactable.currentZonesAndRooms[0].currentZone == "HeavyRooms")
+				{
+					lockedDoors.Add(doorVariant);
+					doorVariant.NetworkTargetState = false;
+					doorVariant.ServerChangeLock(DoorLockReason.NoPower, true);
+				}
+			}
+			Recontainer079.isLocked = true;
+			Utilities.API.Map.TeslaMode = Utilities.API.TeslaMode.DISABLED;
+			foreach (Scp079PlayerScript scp079PlayerScript in Scp079PlayerScript.instances)
+				ps.HurtPlayer(new PlayerStats.HitInfo(1000001f, "WORLD", DamageTypes.Recontainment, 0), scp079PlayerScript.gameObject, true, true);
+			yield return Timing.WaitForSeconds(10);
+			if (SecondsLeft <= -1 || rId != RoundPlus.RoundId)
+			{
+				Handle = null;
+				yield break;
+			}
+			foreach (DoorVariant doorVariant2 in lockedDoors)
+				doorVariant2.ServerChangeLock(DoorLockReason.NoPower, false);
 			Utilities.API.Map.TeslaMode = Utilities.API.TeslaMode.ENABLED;
 			Recontainer079.isLocked = false;
 			Handle = null;
