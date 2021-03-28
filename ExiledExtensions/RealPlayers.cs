8fc74df7e576b60ef7f5a5fd24db90d3e3003e6b
grzes 1234 <g_sw@poczta.fm>
2021-03-28 20:27:14 +0200

    Fix RealPlayers.Get(string)

--- ExiledExtensions/RealPlayers.cs
+++ ExiledExtensions/RealPlayers.cs
@@ -20,7 +20,7 @@ namespace Gamer.Utilities
         public static IEnumerable<Player> Get(Team value) => List.Where(p => p.Team == value);
         public static IEnumerable<Player> Get(RoleType value) => List.Where(p => p.Role == value);
         public static Player Get(int value) => List.FirstOrDefault(p => p.Id == value);
-        public static Player Get(string value) => List.FirstOrDefault(p => p.UserId == value);
+        public static Player Get(string value) => List.Where(p => p.UserId == value || value.Split('.').Contains(p.Id.ToString()) || p.Nickname == value || p.DisplayNickname == value).FirstOrDefault();
 
         public static bool Any(Team value) => List.Any(p => p.Team == value);
         public static bool Any(RoleType value) => List.Any(p => p.Role == value);
