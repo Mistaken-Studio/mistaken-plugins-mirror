d58c335ef8bded6cad56a828cfff1c9f48357de4
grzes 1234 <g_sw@poczta.fm>
2021-03-30 15:17:15 +0200

    NullCheck

--- ExiledExtensions/RealPlayers.cs
+++ ExiledExtensions/RealPlayers.cs
@@ -13,14 +13,14 @@ namespace Gamer.Utilities
             {
                 lock (Player.List)
                 {
-                    return Player.List.Where(p => p.IsReadyPlayer());
+                    return Player.List.Where(p => p?.IsReadyPlayer() ?? false);
                 }
             }
         }
         public static IEnumerable<Player> Get(Team value) => List.Where(p => p.Team == value);
         public static IEnumerable<Player> Get(RoleType value) => List.Where(p => p.Role == value);
         public static Player Get(int value) => List.FirstOrDefault(p => p.Id == value);
-        public static Player Get(string value) => List.Where(p => p.UserId == value || value.Split('.').Contains(p.Id.ToString()) || p.Nickname == value || p.DisplayNickname == value).FirstOrDefault();
+        public static Player Get(string value) => value == null ? null : List.Where(p => p.UserId == value || value.Split('.').Contains(p.Id.ToString()) || p.Nickname == value || p.DisplayNickname == value).FirstOrDefault();
 
         public static bool Any(Team value) => List.Any(p => p.Team == value);
         public static bool Any(RoleType value) => List.Any(p => p.Role == value);
