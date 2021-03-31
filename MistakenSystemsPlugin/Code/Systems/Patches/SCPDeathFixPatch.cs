4b8cf81c259f0de66ad0988cf76842e6ac91de5b
grzes 1234 <g_sw@poczta.fm>
2021-03-31 17:13:37 +0200

    SCP Death fix

---
+++ MistakenSystemsPlugin/Code/Systems/Patches/SCPDeathFixPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RemoteAdmin;
+using HarmonyLib;
+using Assets._Scripts.Dissonance;
+using Respawning.NamingRules;
+using System.Text.RegularExpressions;
+using Exiled.API.Features;
+
+namespace Gamer.Mistaken.Systems.Patches
+{
+    [HarmonyPatch(typeof(NineTailedFoxNamingRule), "GetCassieUnitName")]
+    static class SCPDeathFixPatch
+	{
+		public static bool Prefix(ref string regular)
+		{
+			Log.Debug(regular);
+			if (regular.StartsWith("<color="))
+				regular = regular.Substring(12, regular.Length - 12 - 8);
+			Log.Debug(regular);
+			return true;
+		}
+	}
+}
