01a9fd8f2a027c76a0a98240df0c5c76f8ca8e45
Xname <suszka.kuba@o2.pl>
2021-04-01 17:19:13 +0200

    Impact Grenade commented

--- Xname.ImpactGrenade/PluginHandler.cs
+++ Xname.ImpactGrenade/PluginHandler.cs
@@ -7,19 +7,23 @@ using System.Text;
 
 namespace Xname.ImpactGrenade
 {
+    /// <inheritdoc/>
     public class PluginHandler : Plugin<Config>
     {
+        /// <inheritdoc/>
         public override string Author => "Xname";
+        /// <inheritdoc/>
         public override string Name => "ImpactGrenade";
+        /// <inheritdoc/>
         public override string Prefix => "IMP";
-
+        /// <inheritdoc/>
         public override void OnEnabled()
         {
             new ImpHandler(this);
             Gamer.Diagnostics.Module.OnEnable(this);
             base.OnEnabled();
         }
-
+        /// <inheritdoc/>
         public override void OnDisabled()
         {
             Gamer.Diagnostics.Module.OnDisable(this);
