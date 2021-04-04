a380c40e185b354beb50e38d2679f0e49c93a36b
grzes 1234 <g_sw@poczta.fm>
2021-04-04 17:00:45 +0200

    Infinite Radio on Short Range

---
+++ MistakenSystemsPlugin/Code/Systems/Patches/RadioPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RemoteAdmin;
+using HarmonyLib;
+using UnityEngine;
+using Exiled.API.Features;
+using Gamer.Mistaken.Utilities.APILib;
+using Mirror;
+using MEC;
+using Gamer.Utilities;
+
+namespace Gamer.Mistaken.Systems.Patches
+{
+	[HarmonyPatch(typeof(Radio), "UseBattery")]
+	static class RadioPatch
+	{
+		public static bool Prefix(Radio __instance)
+		{
+			if (__instance.curPreset == 1)
+				return false;
+			return true;
+		}
+	}
+}
