faefd7163d97059a5222bd52e2ddc9da1eace8c2
grzes 1234 <g_sw@poczta.fm>
2021-04-10 09:11:34 +0200

    Disarming fix

--- MistakenSystemsPlugin/Code/Systems/Patches/DisarmPatch.cs
+++ MistakenSystemsPlugin/Code/Systems/Patches/DisarmPatch.cs
@@ -18,15 +18,13 @@ namespace Gamer.Mistaken.Systems.Patches
 	{
 		public static bool Prefix(Handcuffs __instance, GameObject target)
 		{
-			if (!__instance._interactRateLimit.CanExecute(true))
-				return false;
 			if (target == null || Vector3.Distance(target.transform.position, __instance.transform.position) > __instance.raycastDistance * 1.1f)
-				return false;
+				return true;
 			Handcuffs handcuffs = ReferenceHub.GetHub(target).handcuffs;
 			if (handcuffs == null || __instance.MyReferenceHub.inventory.curItem != ItemType.Disarmer || __instance.MyReferenceHub.characterClassManager.CurClass < RoleType.Scp173)
-				return false;
-			if (handcuffs.CufferId < 0 && !__instance.ForceCuff && handcuffs.MyReferenceHub.inventory.curItem == ItemType.None)
-				return false;
+				return true;
+			if (!(handcuffs.CufferId < 0 && !__instance.ForceCuff && handcuffs.MyReferenceHub.inventory.curItem == ItemType.None))
+				return true;
 			if (__instance.MyReferenceHub.characterClassManager.CurRole.team == Team.CDP)
 			{
 				if (handcuffs.MyReferenceHub.characterClassManager.CurRole.team == Team.CDP)
