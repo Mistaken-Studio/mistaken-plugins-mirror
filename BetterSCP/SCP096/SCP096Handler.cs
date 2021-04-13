1b51f3fd1f2c708b5501fa7876878c615b81399a
grzes 1234 <g_sw@poczta.fm>
2021-04-13 16:44:38 +0200

    SCP 096 GUI fix?

--- BetterSCP/SCP096/SCP096Handler.cs
+++ BetterSCP/SCP096/SCP096Handler.cs
@@ -71,7 +71,7 @@ namespace Gamer.Mistaken.BetterSCP.SCP096
                     Mistaken.Systems.GUI.PseudoGUIHandler.Set(player, "scp096", Mistaken.Systems.GUI.PseudoGUIHandler.Position.TOP, null);
                 foreach (var scp096 in RealPlayers.Get(RoleType.Scp096))
                 {
-                    if (scp096.ReferenceHub.scpsController.CurrentScp is PlayableScps.Scp096 scp096script && (scp096script.Enraged || scp096script.Enraging))
+                    if (scp096.Role == RoleType.Scp096 && scp096.ReferenceHub.scpsController.CurrentScp is PlayableScps.Scp096 scp096script && (scp096script.Enraged || scp096script.Enraging))
                     {
                         string targetMessage = plugin.ReadTranslation("Info_096Target", scp096script._targets.Count);
                         foreach (var item in scp096script._targets.ToArray())
