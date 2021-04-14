fc259196e055c3217f011a2f3678bf7634e0c7b7
grzes 1234 <g_sw@poczta.fm>
2021-04-14 11:36:30 +0200

    No AC Breaking Small Size

---
+++ MistakenSystemsPlugin/Code/Systems/Patches/ACSmolPatch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RemoteAdmin;
+using HarmonyLib;
+using UnityEngine;
+using Exiled.API.Features;
+using Gamer.Mistaken.Utilities.APILib;
+using Mirror;
+using Gamer.Mistaken.Systems.Staff;
+using Gamer.Utilities;
+
+namespace Gamer.Mistaken.Systems.Patches
+{
+	[HarmonyPatch(typeof(PlayerMovementSync), "ForcePosition", typeof(Vector3), typeof(string), typeof(bool), typeof(bool), typeof(bool))]
+	static class ACSmolPatch
+	{
+		public static bool Prefix(PlayerMovementSync __instance, Vector3 pos, string anticheatCode, bool reset = false, bool grantSafeTime = true, bool resetPrevSafePositions = true)
+		{
+			if (anticheatCode != "S.7")
+				return true;
+			var scale = Player.Get(__instance.gameObject)?.Scale;
+			if (scale != Vector3.one)
+            {
+				Log.Debug($"[ACSmolPatch] Denied AntyCheat");
+				Log.Debug($"[ACSmolPatch] Scale: {scale}");
+				__instance._lastSafePosition = __instance.RealModelPosition;	
+				return false;
+			}
+			else
+				Log.Debug($"[ACSmolPatch] Allowed AntyCheat");
+			return true;
+		}
+	}
+}
