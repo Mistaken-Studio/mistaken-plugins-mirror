1269462d8b0ae698579d38e8ea98749c964a1641
grzes 1234 <g_sw@poczta.fm>
2021-04-19 09:53:12 +0200

    Part 1

---
+++ MistakenSystemsPlugin/Code/CassieRoom/CassieRoomHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using MEC;
+using System;
+using System.Linq;
+using Gamer.Mistaken.BetterRP.Ambients;
+using Exiled.API.Features;
+using Exiled.API.Extensions;
+using Mirror;
+using Gamer.Utilities;
+using Gamer.Diagnostics;
+
+namespace Gamer.Mistaken.CassieRoom
+{
+    public class CassieRoomHandler : Module
+    {
+        public CassieRoomHandler(PluginHandler plugin) : base(plugin)
+        {
+        }
+
+        public override string Name => "CassieRoom";
+        public override void OnEnable()
+        {
+            Exiled.Events.Handlers.Server.WaitingForPlayers += this.Handle(() => Server_WaitingForPlayers(), "WaitingForPlayers");
+        }
+        public override void OnDisable()
+        {
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= this.Handle(() => Server_WaitingForPlayers(), "WaitingForPlayers");
+        }
+       
+        private void Server_WaitingForPlayers()
+        {
+            
+        }
+    }
+}
