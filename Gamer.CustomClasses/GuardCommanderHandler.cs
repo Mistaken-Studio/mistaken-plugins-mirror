081e7bf8d7f63fc4768034bec63fff882fe0a317
grzes 1234 <g_sw@poczta.fm>
2021-04-21 21:34:54 +0200

    GuardCommander

--- Gamer.CustomClasses/GuardCommanderHandler.cs
+++ Gamer.CustomClasses/GuardCommanderHandler.cs
@@ -12,6 +12,8 @@ using Exiled.API.Enums;
 using Gamer.Mistaken.Systems.Misc;
 using Gamer.Mistaken.Systems;
 using Gamer.RoundLoggerSystem;
+using UnityEngine;
+using Exiled.API.Extensions;
 
 namespace Gamer.CustomClasses
 {
@@ -27,13 +29,67 @@ namespace Gamer.CustomClasses
             Exiled.Events.Handlers.Player.Died += this.Handle<Exiled.Events.EventArgs.DiedEventArgs>((ev) => Player_Died(ev));
             Exiled.Events.Handlers.Player.ChangingRole += this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
             Exiled.Events.Handlers.Server.RoundStarted += this.Handle(() => Server_RoundStarted(), "RoundStart");
+            Exiled.Events.Handlers.Player.InteractingDoor += this.Handle<Exiled.Events.EventArgs.InteractingDoorEventArgs>((ev) => Player_InteractingDoor(ev));
         }
         public override void OnDisable()
         {
-            Exiled.Events.Handlers.Player.Died += this.Handle<Exiled.Events.EventArgs.DiedEventArgs>((ev) => Player_Died(ev));
-            Exiled.Events.Handlers.Player.ChangingRole += this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
-            Exiled.Events.Handlers.Server.RoundStarted += this.Handle(() => Server_RoundStarted(), "RoundStart");
+            Exiled.Events.Handlers.Player.Died -= this.Handle<Exiled.Events.EventArgs.DiedEventArgs>((ev) => Player_Died(ev));
+            Exiled.Events.Handlers.Player.ChangingRole -= this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
+            Exiled.Events.Handlers.Server.RoundStarted -= this.Handle(() => Server_RoundStarted(), "RoundStart");
+            Exiled.Events.Handlers.Player.InteractingDoor -= this.Handle<Exiled.Events.EventArgs.InteractingDoorEventArgs>((ev) => Player_InteractingDoor(ev));
         }
+
+        private void Player_InteractingDoor(Exiled.Events.EventArgs.InteractingDoorEventArgs ev)
+        {
+            if (ev.IsAllowed)
+                return;
+            if (GuardCommander != ev.Player)
+                return;
+            if (ev.Player.CurrentItem.id != ItemType.KeycardSeniorGuard)
+                return;
+            var type = ev.Door.Type();
+            if(type == DoorType.NukeSurface)
+            {
+                foreach (var player in RealPlayers.List.Where(p => p.Id != ev.Player.Id && (p.Role != RoleType.FacilityGuard && p.Team == Team.MTF)))
+                {
+                    if (Vector3.Distance(player.Position, ev.Player.Position) < 10)
+                    {
+                        ev.IsAllowed = true;
+                        return;
+                    }
+                }
+            }
+            else if (type == DoorType.Scp106Primary || type == DoorType.Scp106Secondary || type == DoorType.Scp106Bottom)
+            {
+                if (!Map.IsLCZDecontaminated)
+                    return;
+                bool tmp = false;
+                foreach (var player in RealPlayers.List.Where(p => p.Id != ev.Player.Id && (p.Role == RoleType.NtfCommander || p.Role == RoleType.NtfScientist)))
+                {
+                    if (Vector3.Distance(player.Position, ev.Player.Position) < 10)
+                    {
+                        if (!tmp)
+                            tmp = true;
+                        else
+                        {
+                            ev.IsAllowed = true;
+                        }
+                    }
+                }
+            }
+            else if (type == DoorType.GateA || type == DoorType.GateB)
+            {
+                foreach (var player in RealPlayers.List.Where(p => p.Id != ev.Player.Id && p.Role == RoleType.Scientist || ((p.Role == RoleType.ClassD || p.Role == RoleType.ChaosInsurgency) && p.IsCuffed)))
+                {
+                    if (Vector3.Distance(player.Position, ev.Player.Position) < 10)
+                    {
+                        ev.IsAllowed = true;
+                        return;
+                    }
+                }
+            }
+        }
+
         public static Player GuardCommander;
         private void Player_Died(Exiled.Events.EventArgs.DiedEventArgs ev)
         {
@@ -60,14 +116,14 @@ namespace Gamer.CustomClasses
             {
                 try
                 {
-                    var guards = RealPlayers.Get(RoleType.FacilityGuard).ToArray();
-                    if (guards.Length < 3)
+                    var guards = RealPlayers.Get(/*RoleType.FacilityGuard*/RoleType.ClassD).ToArray();
+                    if (guards.Length < 3 && false)
                         return;
                     GuardCommander = guards[UnityEngine.Random.Range(0, guards.Length)];
                     GuardCommander.SetRole(RoleType.NtfCommander, true, false);
                     GuardCommander.ClearInventory();
-                    GuardCommander.AddItem(ItemType.GunE11SR);
-                    GuardCommander.AddItem(ItemType.KeycardNTFLieutenant);
+                    GuardCommander.AddItem(ItemType.GunProject90);
+                    GuardCommander.AddItem(ItemType.KeycardSeniorGuard);
                     GuardCommander.AddItem(ItemType.Disarmer);
                     GuardCommander.AddItem(ItemType.Radio);
                     ArmorHandler.LiteArmor.Give(GuardCommander, 15);
@@ -78,7 +134,7 @@ namespace Gamer.CustomClasses
                         id = ItemType.WeaponManagerTablet,
                         durability = 1.301f
                     });
-                    GuardCommander.Ammo[(int)AmmoType.Nato556] = 120;
+                    GuardCommander.Ammo[(int)AmmoType.Nato9] = 150;
                     CustomInfoHandler.Set(GuardCommander, "Guard_Commander", "<color=blue><b>Dowódca Ochrony</b></color>", false);
                     RoundLoggerSystem.RoundLogger.Log("CUSTOM CLASSES", "GUARD COMMANDER", $"Spawned {GuardCommander.PlayerToString()} as Guard Commander");
                 }
