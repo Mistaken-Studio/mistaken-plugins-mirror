6079875556366f9a4a5521fa534ed58033a8721e
grzes 1234 <g_sw@poczta.fm>
2021-04-22 17:10:54 +0200

    FakeNick fix

--- MistakenSystemsPlugin/Code/Systems/Patches/NicknamePatch.cs
+++ MistakenSystemsPlugin/Code/Systems/Patches/NicknamePatch.cs
@@ -15,10 +15,12 @@ namespace Gamer.Mistaken.Systems.Patches
 {
 	[HarmonyPatch(typeof(NicknameSync), "UpdateNickname", typeof(string))]
 	[HarmonyPatch(typeof(NicknameSync), "CallCmdSetNick", typeof(string))]
-	static class NicknamePatch
+	public static class NicknamePatch
 	{
+		public static readonly Dictionary<string, string> RealNicknames = new Dictionary<string, string>();
 		public static bool Prefix(NicknameSync __instance, ref string n)
 		{
+			RealNicknames[__instance._hub.characterClassManager.UserId] = n;
 			if (CommandsExtender.Commands.FakeNickCommand.FullNicknames.TryGetValue(__instance._hub.characterClassManager.UserId, out string newNick))
 				n = newNick;
 			return true;
@@ -29,6 +31,7 @@ namespace Gamer.Mistaken.Systems.Patches
 	{
 		public static bool Prefix(NicknameSync __instance, ref string nick)
 		{
+			NicknamePatch.RealNicknames[__instance._hub.characterClassManager.UserId] = nick;
 			if (CommandsExtender.Commands.FakeNickCommand.FullNicknames.TryGetValue(__instance._hub.characterClassManager.UserId, out string newNick))
 				nick = newNick;
 			return true;
