41a566dcef76263255d11e1643d97598bf74f75e
grzes 1234 <g_sw@poczta.fm>
2021-04-26 08:27:09 +0200

    Ambients, but new are disabled

--- MistakenSystemsPlugin/Code/BetterRP/Ambients.cs
+++ MistakenSystemsPlugin/Code/BetterRP/Ambients.cs
@@ -317,67 +317,60 @@ namespace Gamer.Mistaken.BetterRP.Ambients
                             var randPitch = UnityEngine.Random.Range(0.1f, 1.5f);
                             return $"pitch_{randPitch.ToString().Replace(',', '.')} jam_040_2 .g{randGSound}";
                         }
-                    /*case 2:
-                    case 3:
+                    case 2:
                         {
-                            int i = UnityEngine.Random.Range(0, 4);
-                            int max = 0;
-                            switch(i)
-                            {
-                                case 0:
-                                    {
-                                        max = 16;
-                                        break;
-                                    }
-                                case 1:
-                                    {
-                                        max = 12;
-                                        break;
-                                    }
-                                case 2:
-                                    {
-                                        max = 12;
-                                        break;
-                                    }
-                                case 3:
-                                    {
-                                        max = 12;
-                                        break;
-                                    }
-                            } 
-                            CommsHack.AudioAPI.API.PlayFileRaw($"{CommsHack.AudioAPI.BASE_PATH}Ambient/Ambient{i}_{UnityEngine.Random.Range(1, max)}.ogg.raw", 0.5f);
-                            return "";
-                        }*/
+                            string[] items = new string[] 
+                            { 
+                                "pitch_0.1 SCP pitch_0.1 6 pitch_0.1 8 pitch_0.1 2", 
+                                "pitch_0.1 scpsubjects",
+                                "pitch_0.1 Camera",
+                                "pitch_0.1 Celsius",
+                                "pitch_0.1 .g7 h h",
+                                "pitch_0.1 ContainedSuccessfully",
+                                "pitch_0.1 Decontamination",
+                                "pitch_0.1 Intersection",
+                                "pitch_0.1 NineTailedFox",
+                                "pitch_0.1 Personnel h h a c s e",
+                                "pitch_0.1 HasEntered",
+                                "pitch_0.1 e s s",
+                                "pitch_0.1 c c h",
+                                "pitch_0.1 ChaosInsurgency"
+                            };
+                            return items[UnityEngine.Random.Range(0, items.Length)];
+                        }
                     default:
                         return "";
                 }
             }
-        }
+    }
 
         public override bool IsJammed => false;
 
         public override bool IsReusable => true;
 
-        /*public override bool CanPlay()
+        public RandomAmbient()
         {
-            if(base.CanPlay())
+            string[] items = new string[]
             {
-                if(UnityEngine.Random.Range(0, 2) == 0)
-                {
-                    var randGSound = UnityEngine.Random.Range(1, 7);
-                    Cassie.Message($"jam_040_2 .g{randGSound}", false, false);
-                }
-                else
-                {
-                    var randGSound = UnityEngine.Random.Range(1, 7);
-                    var randPitch = UnityEngine.Random.Range(0.1f, 1.5f);
-                    Cassie.Message($"pitch_{randPitch.ToString().Replace(',', '.')} jam_040_2 .g{randGSound}", false, false);
-                }
-            }
-            return false;
-        }*/
+                "pitch_0.1 SCP pitch_0.1 6 pitch_0.1 8 pitch_0.1 2",
+                "pitch_0.1 scpsubjects",
+                "pitch_0.1 Camera",
+                "pitch_0.1 Celsius",
+                "pitch_0.1 .g7 h h",
+                "pitch_0.1 ContainedSuccessfully",
+                "pitch_0.1 Decontamination",
+                "pitch_0.1 Intersection",
+                "pitch_0.1 NineTailedFox",
+                "pitch_0.1 Personnel h h a c s e",
+                "pitch_0.1 HasEntered",
+                "pitch_0.1 e s s",
+                "pitch_0.1 c c h",
+                "pitch_0.1 ChaosInsurgency"
+            };
+            foreach (var item in items)
+                Subtitles.SubtitlesHandler.IgnoredSubtitles.Add(item);
+        }
     }
-
 
     public class Blank : Ambient
     {
