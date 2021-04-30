a2bbdb0795994339776825a1bcf0f861426916a5
grzes 1234 <g_sw@poczta.fm>
2021-04-30 15:52:16 +0200

    Null Check

--- MistakenSystemsPlugin/Systems/Patches/NicknamePatch.cs
+++ MistakenSystemsPlugin/Systems/Patches/NicknamePatch.cs
@@ -10,6 +10,8 @@ namespace Gamer.Mistaken.Systems.Patches
         public static readonly Dictionary<string, string> RealNicknames = new Dictionary<string, string>();
         public static bool Prefix(NicknameSync __instance, ref string n)
         {
+            if (string.IsNullOrWhiteSpace(__instance._hub.characterClassManager.UserId))
+                return true;
             RealNicknames[__instance._hub.characterClassManager.UserId] = n;
             if (CommandsExtender.Commands.FakeNickCommand.FullNicknames.TryGetValue(__instance._hub.characterClassManager.UserId, out string newNick))
                 n = newNick;
@@ -21,6 +23,8 @@ namespace Gamer.Mistaken.Systems.Patches
     {
         public static bool Prefix(NicknameSync __instance, ref string nick)
         {
+            if (string.IsNullOrWhiteSpace(__instance._hub.characterClassManager.UserId))
+                return true;
             NicknamePatch.RealNicknames[__instance._hub.characterClassManager.UserId] = nick;
             if (CommandsExtender.Commands.FakeNickCommand.FullNicknames.TryGetValue(__instance._hub.characterClassManager.UserId, out string newNick))
                 nick = newNick;
