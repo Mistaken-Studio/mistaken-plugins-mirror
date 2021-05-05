98cdfe5dd1926cb49b9efe30b05592447fc039eb
grzes 1234 <g_sw@poczta.fm>
2021-05-05 15:16:03 +0200

    Patches

--- Gamer.CITester/PluginHandler.cs
+++ Gamer.CITester/PluginHandler.cs
@@ -364,4 +364,26 @@ namespace Gamer.CITester
             return false;
         }
     }
+
+    [HarmonyPatch(typeof(QueryProcessor), nameof(QueryProcessor.TargetSyncGameplayData))]
+    internal static class QueryProcessor_TargetSyncGameplayData
+    {
+        private static bool Prefix(NetworkConnection conn, bool gd)
+        {
+            if (conn == null)
+                return false;
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(VersionCheck), nameof(VersionCheck.TargetCheckVersion))]
+    internal static class VersionCheck_TargetCheckVersion
+    {
+        private static bool Prefix(NetworkConnection conn, byte major, byte minor, byte revison)
+        {
+            if (conn == null)
+                return false;
+            return true;
+        }
+    }
 }
