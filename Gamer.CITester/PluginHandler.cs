320ac912b414d45b0c58cc8d448690e643b12b8a
grzes 1234 <g_sw@poczta.fm>
2021-05-05 13:13:22 +0200

    Fix.ree

--- Gamer.CITester/PluginHandler.cs
+++ Gamer.CITester/PluginHandler.cs
@@ -359,7 +359,7 @@ namespace Gamer.CITester
     [HarmonyPatch(typeof(PlayableScps.Scp096), nameof(PlayableScps.Scp096.OnDamage))]
     internal static class PlayableScps_Scp096_OnDamage
     {
-        private static bool Prefix(GameObject target)
+        private static bool Prefix(PlayerStats.HitInfo info)
         {
             return false;
         }
