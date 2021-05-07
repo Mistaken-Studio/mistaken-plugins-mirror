1f067d6487e30a97bf23ed6a4b5db5d0f602bedc
grzes 1234 <g_sw@poczta.fm>
2021-05-07 15:19:46 +0200

    Speed.exe

--- MistakenSystemsPlugin/Systems/Patches/ACSmolPatch.cs
+++ MistakenSystemsPlugin/Systems/Patches/ACSmolPatch.cs
@@ -3,6 +3,7 @@
 using Exiled.API.Features;
 using Gamer.Utilities;
 using HarmonyLib;
+using System;
 using UnityEngine;
 
 namespace Gamer.Mistaken.Systems.Patches
@@ -35,6 +36,7 @@ namespace Gamer.Mistaken.Systems.Patches
                 case "R.2":
                 case "S.1":
                     {
+                        return true;
                         Log.Debug($"[ACSmolPatch] Code: {anticheatCode}", DEBUG);
                         var player = Player.Get(__instance.gameObject);
                         if (player == null)
@@ -50,6 +52,7 @@ namespace Gamer.Mistaken.Systems.Patches
                             Log.Debug($"[ACSmolPatch] Walk: {player.GetSessionVar<float>(Main.SessionVarType.RUN_SPEED, ServerConfigSynchronizer.Singleton.NetworkHumanSprintSpeedMultiplier)}", DEBUG);
                             __instance._realModelPosition = __instance.transform.position;
                             __instance._lastSafePosition = __instance.RealModelPosition;
+
                             return false;
                         }
                         else
@@ -59,6 +62,55 @@ namespace Gamer.Mistaken.Systems.Patches
                 default:
                     return true;
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(FirstPersonController), "GetSpeed", new Type[] { typeof(float), typeof(bool) }, new ArgumentType[] { ArgumentType.Out, ArgumentType.Normal })]
+    static class SpeedPatch
+    {
+        public static bool Prefix(FirstPersonController __instance, ref float speed, bool isServerSide)
+        {
+            if (!isServerSide)
+                return true;
+            if (__instance._hub.characterClassManager.IsAnyScp())
+                return true;
+            var player = Player.Get(__instance.gameObject);
+            if (player == null)
+                return true;
+            float sprintSpeed = player.GetSessionVar<float>(Main.SessionVarType.RUN_SPEED, ServerConfigSynchronizer.Singleton.HumanSprintSpeedMultiplier);
+            float walkSpeed = player.GetSessionVar<float>(Main.SessionVarType.WALK_SPEED, ServerConfigSynchronizer.Singleton.HumanWalkSpeedMultiplier);
+            __instance.curRole = __instance._hub.characterClassManager.Classes.SafeGet(__instance._hub.characterClassManager.CurClass);
+            __instance.IsSneaking = __instance._hub.animationController.MoveState == PlayerMovementState.Sneaking;
+            speed = (__instance.staminaController.AllowMaxSpeed ? __instance.curRole.runSpeed : __instance.curRole.walkSpeed);
+            speed *= (__instance.staminaController.AllowMaxSpeed ? sprintSpeed : walkSpeed);
+            if (__instance.IsSneaking || __instance.ZoomSlowdown < 1f)
+                speed *= Math.Min(__instance.sneakingMultiplier, __instance.ZoomSlowdown);
+            if (__instance.effectScp207.Enabled)
+            {
+                float speedMultiplier = __instance.effectScp207.GetSpeedMultiplier();
+                if (speedMultiplier > 1f && !__instance.IsSneaking)
+                {
+                    if (__instance.IsWalking)
+                        speed = __instance.curRole.runSpeed;
+                    else
+                        speed = __instance.curRole.runSpeed * speedMultiplier;
+                }
+            }
+            if (__instance.effectSinkhole.Enabled)
+            {
+                float num = 1f - __instance.effectSinkhole.slowAmount / 100f;
+                speed = Mathf.Max(num * speed, num * __instance.curRole.walkSpeed);
+                __instance.IsSprinting = false;
+            }
+            if (__instance.effectCorroding.Enabled)
+                speed = Mathf.Clamp(speed, 0f, __instance.curRole.walkSpeed);
+            __instance.IsSprinting = false;
+            if (__instance.effectDisabled.Enabled)
+                speed *= __instance.effectDisabled.SpeedMultiplier;
+            if (__instance.effectEnsnared.Enabled)
+                speed = 0f;
+            speed *= __instance._hub.weaponManager.SpeedMultiplier;
+            return false;
         }
     }
 }
