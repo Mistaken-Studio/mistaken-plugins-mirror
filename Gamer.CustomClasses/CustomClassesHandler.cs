88967ab700e327f2ee3ed6a9b0c8abbca8175de2
grzes 1234 <g_sw@poczta.fm>
2021-05-07 17:22:57 +0200

    Yes

--- Gamer.CustomClasses/CustomClassesHandler.cs
+++ Gamer.CustomClasses/CustomClassesHandler.cs
@@ -17,12 +17,30 @@ namespace Gamer.CustomClasses
         {
             Exiled.Events.Handlers.Player.Died += this.Handle<Exiled.Events.EventArgs.DiedEventArgs>((ev) => Player_Died(ev));
             Exiled.Events.Handlers.Player.ChangingRole += this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
+            Exiled.Events.Handlers.Player.Escaping += this.Handle<Exiled.Events.EventArgs.EscapingEventArgs>((ev) => Player_Escaping(ev));
         }
         /// <inheritdoc/>
         public override void OnDisable()
         {
             Exiled.Events.Handlers.Player.Died -= this.Handle<Exiled.Events.EventArgs.DiedEventArgs>((ev) => Player_Died(ev));
             Exiled.Events.Handlers.Player.ChangingRole -= this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
+            Exiled.Events.Handlers.Player.Escaping -= this.Handle<Exiled.Events.EventArgs.EscapingEventArgs>((ev) => Player_Escaping(ev));
+        }
+
+        private void Player_Escaping(Exiled.Events.EventArgs.EscapingEventArgs ev)
+        {
+            if (!ev.IsAllowed)
+                return;
+            if (ev.Player.GetSessionVar<bool>(Main.SessionVarType.CC_IGNORE_CHANGE_ROLE))
+                return;
+            foreach (var item in API.CustomClass.CustomClass.CustomClasses)
+            {
+                if (item.PlayingAsClass.Contains(ev.Player))
+                {
+                    item.OnDie(ev.Player);
+                    break;
+                }
+            }
         }
 
         private void Player_Died(Exiled.Events.EventArgs.DiedEventArgs ev)
@@ -39,6 +57,8 @@ namespace Gamer.CustomClasses
 
         private void Player_ChangingRole(Exiled.Events.EventArgs.ChangingRoleEventArgs ev)
         {
+            if (ev.IsEscaped)
+                return;
             if (ev.Player.GetSessionVar<bool>(Main.SessionVarType.CC_IGNORE_CHANGE_ROLE))
                 return;
             foreach (var item in API.CustomClass.CustomClass.CustomClasses)
