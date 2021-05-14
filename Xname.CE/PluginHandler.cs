16f32320a96baa35f948495aeae6a80fef66b453
Xname <xnameserver@o2.pl>
2021-05-14 22:58:08 +0200

    Combat Extended

---
+++ Xname.CE/PluginHandler.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using Gamer.API;
+
+namespace Xname.CE
+{
+    /// <inheritdoc/>
+    public class PluginHandler : Plugin<Config>
+    {
+        /// <inheritdoc/>
+        public override string Author => "Xname";
+        /// <inheritdoc/>
+        public override string Name => "ColorfullEZ";
+        /// <inheritdoc/>
+        public override string Prefix => "color_ez";
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            new CEHandler(this);
+
+            Gamer.Diagnostics.Module.OnEnable(this);
+            base.OnEnabled();
+        }
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            Gamer.Diagnostics.Module.OnDisable(this);
+            base.OnDisabled();
+        }
+    }
+}
