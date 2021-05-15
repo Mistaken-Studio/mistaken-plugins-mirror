a83777e3a9f0a535774c4e7529af1c74c84243fc
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-05-15 10:51:56 +0200

    Fix

--- Gamer.RaceGameMode/PluginHandler.cs
+++ Gamer.RaceGameMode/PluginHandler.cs
@@ -15,13 +15,13 @@ namespace Gamer.RaceGameMode
         public override string Name => "RaceGameMode";
         public override void OnDisabled()
         {
-            new RaceModule(this);
-
-            Diagnostics.Module.OnEnable(this);
             base.OnDisabled();
         }
         public override void OnEnabled()
         {
+            new RaceModule(this);
+
+            Diagnostics.Module.OnEnable(this);
             base.OnEnabled();
         }
     }
