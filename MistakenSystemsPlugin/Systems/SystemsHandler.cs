cf2adf5f6b5f1c70b217db2584cac849f0b2fbd5
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-05-18 14:14:24 +0200

    Testing

--- MistakenSystemsPlugin/Systems/SystemsHandler.cs
+++ MistakenSystemsPlugin/Systems/SystemsHandler.cs
@@ -612,14 +612,10 @@ namespace Gamer.Mistaken.Systems
 
             Logs.LogManager.RoundStartTime[RoundPlus.RoundId] = DateTime.Now;
             foreach (var item in Logs.LogManager.DoorLogs)
-            {
                 NorthwoodLib.Pools.ListPool<Logs.DoorLog>.Shared.Return(item.Value);
-            }
             Logs.LogManager.DoorLogs.Clear();
             foreach (var item in Logs.LogManager.ElevatorLogs)
-            {
                 NorthwoodLib.Pools.ListPool<Logs.ElevatorLog>.Shared.Return(item.Value);
-            }
             Logs.LogManager.ElevatorLogs.Clear();
             Logs.LogManager.FlashLog.Clear();
 
@@ -633,6 +629,7 @@ namespace Gamer.Mistaken.Systems
 
             foreach (var item in RealPlayers.List)
             {
+                Log.Debug($"{item.Nickname}: {Systems.Handler.PlayerPreferencesDict[item.UserId].HasFlag(PlayerPreferences.DISABLE_FAST_ROUND_RESTART)}");
                 if(Systems.Handler.PlayerPreferencesDict[item.UserId].HasFlag(PlayerPreferences.DISABLE_FAST_ROUND_RESTART))
                     MirrorExtensions.SendFakeTargetRpc(item, item.ReferenceHub.networkIdentity, typeof(PlayerStats), nameof(PlayerStats.RpcRoundrestart), 10, true);
             }
