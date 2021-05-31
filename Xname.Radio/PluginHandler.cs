d31ee41ce851e6479588c87237581f12cbcfbf5e
Xname <xnameserver@o2.pl>
2021-05-31 17:08:37 +0200

    Radio

---
+++ Xname.Radio/PluginHandler.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Features;
+using Gamer.API;
+
+namespace Xname.Radio
+{
+    /// <inheritdoc/>
+    public class PluginHandler : Plugin<Config>
+    {
+        /// <inheritdoc/>
+        public override string Author => "Xname";
+        /// <inheritdoc/>
+        public override string Name => "Radio";
+        /// <inheritdoc/>
+        public override string Prefix => "radio";
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            new RadioHandler(this);
+            Gamer.Diagnostics.Module.OnEnable(this);
+            base.OnEnabled();
+        }
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            Gamer.Diagnostics.Module.OnDisable(this);
+            base.OnDisabled();
+        }
+    }
+}
