7c6f2d21d2a0cd3ba1af6febc9a7ee8397fbd1da
Xname <xnameserver@o2.pl>
2021-06-05 19:53:46 +0200

    yes

---
+++ Xname.Radio/HelmetsHandler.cs
@@ -0,0 +1,38 @@
+﻿using Exiled.API.Extensions;
+using Exiled.API.Interfaces;
+using Gamer.Diagnostics;
+using Gamer.Mistaken.Base.Staff;
+using Gamer.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Xname.Radio
+{
+    internal class HelmetsHandler : Module
+    {
+        /// <inheritdoc/>
+        public HelmetsHandler(IPlugin<IConfig> plugin) : base(plugin)
+        {
+        }
+        /// <inheritdoc/>
+        public override string Name => "RadioHandler";
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole += this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
+        }
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole -= this.Handle<Exiled.Events.EventArgs.ChangingRoleEventArgs>((ev) => Player_ChangingRole(ev));
+        }
+        private void Player_ChangingRole(Exiled.Events.EventArgs.ChangingRoleEventArgs ev)
+        {
+            ev.Player.GameObject.transform.parent = ItemType.Flashlight.Spawn(0f, ev.Player.Position + Vector3.up, ev.Player.GameObject.transform.rotation).transform;
+        }
+    }
+}
