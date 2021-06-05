64ede143367c881731dc7026d8a87a2f098b4352
Xname <xnameserver@o2.pl>
2021-06-05 21:43:55 +0200

    something

--- Xname.Radio/HelmetsHandler.cs
+++ Xname.Radio/HelmetsHandler.cs
@@ -4,6 +4,7 @@ using Exiled.API.Interfaces;
 using Gamer.Diagnostics;
 using Gamer.Mistaken.Base.Staff;
 using Gamer.Utilities;
+using Grenades;
 using Mirror;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,8 @@ namespace Xname.Radio
 {
     internal class HelmetsHandler : Module
     {
+        internal EffectGrenade effect = new EffectGrenade();
+        internal bool flame = false;
         /// <inheritdoc/>
         public HelmetsHandler(IPlugin<IConfig> plugin) : base(plugin)
         {
@@ -34,7 +37,10 @@ namespace Xname.Radio
         }
         private void Player_ChangedRole(Exiled.Events.EventArgs.ChangedRoleEventArgs ev)
         {
-            var inv = Server.Host.Inventory;
+            if (!flame)
+                UpdateFlame(ev.Player);
+            flame = true;
+            /*var inv = Server.Host.Inventory;
             GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(inv.pickupPrefab, ev.Player.CameraTransform);
             gameObject.transform.rotation = Quaternion.identity;
             //gameObject.transform.localPosition = new Vector3(0, 0.2f, 0);
@@ -45,7 +51,15 @@ namespace Xname.Radio
             gameObject.GetComponent<Pickup>().SetupPickup(ItemType.Flashlight, 0, inv.gameObject, new Pickup.WeaponModifiers(true, 0, 0, 0), Vector3.zero, Quaternion.identity);
             gameObject.transform.localPosition = Vector3.zero;
             gameObject.transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
-            this.CallDelayed(1f, () => ev.Player.SendConsoleMessage($"{gameObject.transform.position}", "green"), "helmetdelay");
+            this.CallDelayed(1f, () => ev.Player.SendConsoleMessage($"{gameObject.transform.position}", "green"), "helmetdelay");*/
+        }
+        private IEnumerator<float> UpdateFlame(Player player)
+        {
+            while (player.IsAlive)
+            {
+                UnityEngine.Object.Instantiate<GameObject>(effect.serverGrenadeEffect, player.Position, Quaternion.identity);
+                yield return 1f;
+            }
         }
     }
 }   
